1138cb353b7db414ef8e21389c104b947744a7f1
sreekar-n <hridaythapa101@gmail.com>
2025-02-04 10:56:39 +0530

    day 3

---
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/Sub2.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.wipro.java.oops.polymorphism;
+
+/**
+ * 
+ */
+public class Sub2 extends TwoNums{
+
+	/**
+	 * 
+	 */
+	//Polymorphism
+	@Override
+	public int operation(int a,int b) {
+		return a-b;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwoNums obj=new Sub2();
+		obj.setA(10);
+		obj.setB(5);
+		int result = obj.operation(obj.getA(), obj.getB());
+		System.out.println("Result: " + result);
+		
+	}
+
+}
